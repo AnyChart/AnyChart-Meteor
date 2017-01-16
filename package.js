aab4483edf378e0c16ab9cddb2586c0f6acc00e2
Alexander Kudryavtsev <clear.meltdown@gmail.com>
2017-01-16 13:16:25 +0800

    Publishing fix

--- package.js
+++ package.js
@@ -1,19 +1,15 @@
 Package.describe({
   name: 'anychart:anychart-meteor',
+  summary: 'The package provides a simple way to pull in AnyChart component in Meteor',
   version: '0.0.1',
-  summary: 'The package provides a simple way to pull in AnyChart component in Meteor.',
   git: 'https://github.com/AnyChart/AnyChart-Meteor',
-  documentation: 'README.md',
-
-  //TODO: Remove this on publish.
-  testOnly: true,
-  debugOnly:true
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   // api.versionsFrom('1.4.2.3');
-  api.use('ecmascript');
-  api.mainModule('anychart-meteor.js');
+  api.mainModule('anychart-meteor.js', 'client');
+  api.export('anychart', 'client');
 });
 
 Npm.depends({
